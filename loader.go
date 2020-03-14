f47b4a50dae736293d96188307a6febdc1704d83
LaysDragon <laysdra7265@gmail.com>
2020-03-14 18:33:07 +0800

    simplify usage though dynamic-struct

--- loader.go
+++ loader.go
@@ -1,19 +1,47 @@
 package loader
 
 import (
+	"github.com/ompluscator/dynamic-struct"
 	"go.mongodb.org/mongo-driver/bson"
 	"golang.org/x/xerrors"
+	"reflect"
 )
 
-var LoaderNotFound = xerrors.New("cannot found loader handlers for src version")
-var TransformerNotFound = xerrors.New("cannot found next transformer to the target version")
-var LoaderSrcTypeIncorrect = xerrors.New("cannot cast HasVersion to target version type")
-var NoVersionTag = xerrors.New("data not container version field")
+var LoaderNotFoundError = xerrors.New("cannot found loader handlers for src version")
+var TransformerNotFoundError = xerrors.New("cannot found next transformer to the target version")
+var TransformerSrcTypeIncorrectError = xerrors.New("src type for transformer is incorrect and failed to cast")
+var NoVersionTagError = xerrors.New("data not version field or with invalid 0.0 version")
+
+//var NoVersionWrapperError = xerrors.New("can't find version wrapper for specified type,this shouldn't happen,check how you init LoaderRegistry with NewLoaderRegistry")
 
 type D interface{}
 type VersionCapture struct {
 	Version Version
 	D
+}
+type AVersionCapture VersionCapture
+
+func (v VersionCapture) MarshalBSON() ([]byte, error) {
+	if v.GetData() != nil {
+		versionWrapper := GetVersionWrapperStruct(v.GetData()).New()
+		reflect.ValueOf(versionWrapper).Elem().FieldByName("Data").Set(reflect.ValueOf(v.GetData()))
+		reflect.ValueOf(versionWrapper).Elem().FieldByName("Version").Set(reflect.ValueOf(v.GetVersion()))
+
+		return bson.Marshal(versionWrapper)
+	}
+	return bson.Marshal(AVersionCapture(v))
+}
+
+func (v *VersionCapture) UnmarshalBSON(src []byte) error {
+	if v.GetData() != nil {
+		versionWrapper := GetVersionWrapperStruct(v.GetData()).New()
+		bson.Unmarshal(src, versionWrapper)
+
+		reader := dynamicstruct.NewReader(versionWrapper)
+		v.SetVersion(reader.GetField("Version").Interface().(Version))
+		v.SetData(reader.GetField("Data").Interface())
+	}
+	return bson.Unmarshal(src, (*AVersionCapture)(v))
 }
 
 func (v *VersionCapture) SetVersion(vv Version) {
@@ -87,6 +115,26 @@ func (t STransformers) SrcToTargetTransformers() SrcToTargetTransformers {
 	return s
 }
 
+var VersionWrapperStructs = map[reflect.Type]dynamicstruct.DynamicStruct{}
+
+func AddVersionWrapperType(typeVal interface{}) dynamicstruct.DynamicStruct {
+	wrapper := dynamicstruct.NewStruct().
+		AddField("Version", Version{}, "").
+		AddField("Data", typeVal, `bson:",inline"`).
+		Build()
+	VersionWrapperStructs[reflect.TypeOf(typeVal)] = wrapper
+	return wrapper
+}
+
+func GetVersionWrapperStruct(typeVal interface{}) dynamicstruct.DynamicStruct {
+	if wrapper, ok := VersionWrapperStructs[reflect.TypeOf(typeVal)]; ok {
+		return wrapper
+	} else {
+		return AddVersionWrapperType(typeVal)
+	}
+
+}
+
 func NewLoaderRegistry(loadersL SLoaders, transformersT STransformers) *LoaderRegistry {
 	l := &LoaderRegistry{
 		SrcLoaders{},
@@ -105,23 +153,24 @@ func NewLoaderRegistry(loadersL SLoaders, transformersT STransformers) *LoaderRe
 			l.versions[srcVersion] = append(l.versions[srcVersion], targetVersion)
 		}
 	}
+
 	return l
 }
 
 func (l *LoaderRegistry) Transform(data HasVersion, target Version) error {
 
 	if data.GetVersion().Greater(target) {
-		return xerrors.Errorf("Raise error from trying donwngrading version %s to %s for %STransformers,please update your target struct version to lastest:%w", data.GetVersion(), target, data, TransformerNotFound)
+		return xerrors.Errorf("Raise error from trying donwngrading version %s to %s for %STransformers,please update your target struct version to lastest:%w", data.GetVersion(), target, data, TransformerNotFoundError)
 	}
 	for data.GetVersion() != target {
 
 		targetVersions, ok := l.versions[data.GetVersion()]
 		if !ok {
-			return xerrors.Errorf("Raise error from version %s to %s for %STransformers:%w", data.GetVersion(), target, data, TransformerNotFound)
+			return xerrors.Errorf("Raise error from version %s to %s for %STransformers:%w", data.GetVersion(), target, data, TransformerNotFoundError)
 		}
 		targetTransformers, ok := l.transformers[data.GetVersion()]
 		if !ok {
-			return xerrors.Errorf("Raise error from version %s to %s for %STransformers:%w", data.GetVersion(), target, data, TransformerNotFound)
+			return xerrors.Errorf("Raise error from version %s to %s for %STransformers:%w", data.GetVersion(), target, data, TransformerNotFoundError)
 		}
 
 		var nextVersion Version
@@ -139,14 +188,12 @@ func (l *LoaderRegistry) Transform(data HasVersion, target Version) error {
 }
 
 func (l *LoaderRegistry) Load(src []byte, target Version) (HasVersion, error) {
-	versionCapture := VersionCapture{
-		D: struct{}{},
-	}
+	versionCapture := VersionCapture{}
 	if err := bson.Unmarshal(src, &versionCapture); err != nil {
 		return nil, err
 	}
 	if (VersionCapture{}) == versionCapture {
-		return nil, xerrors.Errorf("Raise error %w", NoVersionTag)
+		return nil, xerrors.Errorf("Raise error %w", NoVersionTagError)
 	}
 	var processingTarget HasVersion
 	for version, loader := range l.loaders {
@@ -159,7 +206,7 @@ func (l *LoaderRegistry) Load(src []byte, target Version) (HasVersion, error) {
 		}
 	}
 	if processingTarget == nil {
-		return nil, xerrors.Errorf("Raise error from src version %s:%w", versionCapture.Version, LoaderNotFound)
+		return nil, xerrors.Errorf("Raise error from src version %s:%w", versionCapture.Version, LoaderNotFoundError)
 	}
 	if err := l.Transform(processingTarget, target); err != nil {
 		return nil, err
